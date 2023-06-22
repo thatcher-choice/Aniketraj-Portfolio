e7aae591f12f20d154610b94a5bc8438abd716f5
Aniket Jha <f20180068@pilani.bits-pilani.ac.in>
2023-06-23 04:34:54 +0530

    camera changes

--- main.js
+++ main.js
@@ -1,54 +1,117 @@
 import './style.css'
 
 import * as THREE from 'three';
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
-const scene =  new THREE.Scene();
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
+const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/ window.innerHeight, 0.1, 1000); // fov, aspect ratio, view frustum
-const renderer =  new THREE.WebGLRenderer({ //to render out the actual graphic
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000); // fov, aspect ratio, view frustum
+const renderer = new THREE.WebGLRenderer({ //to render out the actual graphic
   canvas: document.querySelector('#bg'),
 });
+var light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(100, 31, 31);
+var light2 =new THREE.DirectionalLight(0xffffff, 1); 
+light2.position.set
+scene.add(light);
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(30);
-camera.position.setX(-3);
-renderer.render(scene , camera);
+// 0.9434 0.9434 47.17
+camera.position.setZ(47.17);
+camera.position.setX(0.9434);
+camera.position.setY(2);
+renderer.render(scene, camera);
+var loader = new GLTFLoader();
+loader.load(
+  'blackhole.glb',
+  function (gltf) {
+    scene.add(gltf.scene);
+  },
+  function (xhr) {
+    console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+  },
+  function (error) {
+    console.error('Error loading GLTF file:', error);
+  }
+);
+// Assuming you have already set up the Three.js scene and camera
 
-const geometry = new THREE.TorusGeometry(10, 3 ,16, 100);  // set of vectors that define the object itself
-const material = new THREE.MeshStandardMaterial({color:0xff6347});
+// Position of the black hole
+const blackHolePosition = new THREE.Vector3(0, 0, 0); // Update with the actual position
+
+// Set up a scroll event listener
+// window.addEventListener('scroll', function () {
+//   // Get the scroll position
+//   const scrollPosition = window.scrollY;
+
+//   // Calculate the distance from the black hole
+//   const distance = camera.position.distanceTo(blackHolePosition);
+
+//   // Assuming you want to trigger the redirection when the camera reaches the center of the black hole
+//   // You can adjust the threshold value as per your requirements
+//   const threshold = 1;
+
+//   // Check if the camera is within the threshold distance of the black hole center
+//   if (distance < threshold && scrollPosition > 0) {
+//     // Redirect to the "projects" page
+//     window.location.href = 'project.html'; // Update with the actual URL of your projects page
+//   }
+// });
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);  // set of vectors that define the object itself
+const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
 const toros = new THREE.Mesh(geometry, material);
 scene.add(toros);
-const pointLight = new THREE.PointLight(0xffffff); 
-pointLight.position.set(5,5,5);
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(1,1,1);
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight);
+window.addEventListener('load', function() {
+  // Scroll to the bottom of the page
+  window.scrollTo(0, document.body.scrollHeight);
+  const farthestDistance = Math.max(
+    Math.abs(camera.position.x),
+    Math.abs(camera.position.y),
+    Math.abs(camera.position.z)
+  );
 
+  // Set the camera position to the farthest distance
+  camera.position.set(
+    camera.position.x * farthestDistance,
+    camera.position.y * farthestDistance,
+    camera.position.z * farthestDistance
+  );
+  if (performance.navigation.type === 1) {
+    // Append a unique query parameter to the URL
+    window.location.href = window.location.href + '?reload=' + Date.now();
+  }
+});
 // const lightHelper = new THREE.PointLightHelper(pointLight);
 // const gridHelper = new  THREE.GridHelper(200,50);
 // scene.add(lightHelper, gridHelper);
 // const controls =new OrbitControls(camera, renderer.domElement);
 
-function addStar(){
+function addStar() {
   const geometry = new THREE.SphereGeometry(0.05, 24, 24);
-  const material =new THREE.MeshStandardMaterial({color: 0xffffff})
-  const star = new THREE.Mesh( geometry, material);
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff })
+  const star = new THREE.Mesh(geometry, material);
 
-  const [x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
-  star.position.set(x,y,z);
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+  star.position.set(x, y, z);
   scene.add(star);
 }
 
 Array(5000).fill().forEach(addStar);
 // background
-const spaceTexture = new THREE.TextureLoader().load('space.jpg'); 
+const spaceTexture = new THREE.TextureLoader().load('space.jpg');
 scene.background = spaceTexture;
- 
+
 //anni
 const anniTexture = new THREE.TextureLoader().load('anni.jpg');
 
 const anni = new THREE.Mesh(
-  new THREE.BoxGeometry(3,3,3),
+  new THREE.BoxGeometry(3, 3, 3),
   new THREE.MeshStandardMaterial({
     map: anniTexture
   })
@@ -57,7 +120,7 @@ const anni = new THREE.Mesh(
 scene.add(anni)
 //mooon
 
-const moonTexture =new THREE.TextureLoader().load('moon.jpg');
+const moonTexture = new THREE.TextureLoader().load('moon.jpg');
 const normalTexture = new THREE.TextureLoader().load('normal.jpg');
 const moon = new THREE.Mesh(
   new THREE.SphereGeometry(3, 32, 32),
@@ -68,7 +131,7 @@ const moon = new THREE.Mesh(
 );
 scene.add(moon);
 
-moon.position.z=30;
+moon.position.z = 30;
 moon.position.setX(-10);
 
 anni.position.z = -5;
@@ -78,25 +141,27 @@ function moveCamera() {
   const t = document.body.getBoundingClientRect().top;
   moon.rotation.x += 0.05;
   moon.rotation.y += 0.075;
-  moon.rotation.z += 0.05; 
+  moon.rotation.z += 0.05;
 
-  anni.rotation.y +=0.01;
-  anni.rotation.z +=0.01;
+  anni.rotation.y += 0.01;
+  anni.rotation.z += 0.01;
 
   camera.position.z = t * -0.01;
   camera.position.x = t * -0.0002;
   camera.rotation.y = t * -0.0002;
+
+  console.log(camera.position.x,camera.rotation.y,camera.position.z );
 }
 document.body.onscroll = moveCamera;
 moveCamera();
-function animate(){
+function animate() {
   requestAnimationFrame(animate);
-  toros.rotation.x += 0.01; 
+  toros.rotation.x += 0.01;
   toros.rotation.y += 0.005;
   toros.rotation.z += 0.01;
 
   moon.rotation.x += 0.005;
-  anni.rotation.x +=0.005;
+  anni.rotation.x += 0.005;
   // controls.update();
   renderer.render(scene, camera);
 }
